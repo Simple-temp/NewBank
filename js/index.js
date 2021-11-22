1256785f3b295b47af5479849fecd431bc657ac8
Simple-temp <fiveriam01@gmail.com>
2021-11-22 06:55:35 -1100

    added new code

--- js/index.js
+++ js/index.js
@@ -1,7 +1,47 @@
+/*get email and pass*/
 let getBtn = document.querySelector(".login-btn");
 getBtn.addEventListener("click",function(){
+    let isEmail = "example@Email.com";
+    let isPass = 1234;
+    let getEmail = document.querySelector("#getEmail").value;
+    let getPass = document.querySelector("#getPass").value;
+    let Massage = document.querySelector("#massage");
+    let MassageHack = document.querySelector("#massagehack");
     let hideDiv = document.querySelector(".login-box");
-    let showDiv = document.querySelector(".next");
-    hideDiv.style.display = "none";
-    showDiv.style.display = "block";
+    let showDiv = document.querySelector(".NextPage");
+    if (isEmail==getEmail && isPass==getPass)
+    {
+        hideDiv.style.display = "none";
+        showDiv.style.display = "block";
+        MassageHack.innerHTML = "Hack korar ki khub dorkar chilo ne ja ache shob niye jaa";
+        MassageHack.style.color = "red";
+        MassageHack.style.fontSize = "14px";
+        MassageHack.style.textTransform = "uppercase";
+        MassageHack.style.fontWeight = "bold";
+    } 
+    else if (getPass=="")
+    {
+        Massage.innerHTML = "Hack korish na vai kissu nai ..<br> email r password ta dee";
+        Massage.style.color = "red";
+        Massage.style.fontSize = "14px";
+        Massage.style.textTransform = "uppercase";
+        Massage.style.fontWeight = "bold";
+    }
+    else if (getEmail=="")
+    {
+        Massage.innerHTML = "Hack korish na vai kissu nai ..<br> email r password ta dee";
+        Massage.style.color = "red";
+        Massage.style.fontSize = "14px";
+        Massage.style.textTransform = "uppercase";
+        Massage.style.fontWeight = "bold";
+    }
+    else 
+    {
+        Massage.innerHTML = "Hack korish na vai kissu nai ..<br> email r password ta dee";
+        Massage.style.color = "red";
+        Massage.style.fontSize = "14px";
+        Massage.style.textTransform = "uppercase";
+        Massage.style.fontWeight = "bold";
+    }
 });
+
