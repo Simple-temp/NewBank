0279f5e563c025a531939b69d8fb5e55b5a3da43
Simple-temp <fiveriam01@gmail.com>
2021-11-23 01:05:22 -1100

    complete coding

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 /*get email and pass*/
 let getBtn = document.querySelector(".login-btn");
 getBtn.addEventListener("click",function(){
-    let isEmail = "example@Email.com";
+    let isEmail = "example@gmail.com";
     let isPass = 1234;
     let getEmail = document.querySelector("#getEmail").value;
     let getPass = document.querySelector("#getPass").value;
@@ -44,4 +44,58 @@ getBtn.addEventListener("click",function(){
         Massage.style.fontWeight = "bold";
     }
 });
+
+/*deposit button handler*/
+
+const depositBtn = document.getElementById("depositAmountBtn");
+depositBtn.addEventListener("click",function(){
+
+        const depositNum = getInputNumber("depositAmount");
+
+            updateSpanText("currentDeposit",depositNum);
+    
+            updateSpanText("currentBalance",depositNum);
+        
+        
+            document.getElementById("depositAmount").value ="";
+})
+
+
+/*withdrow button handler*/
+
+const withdrowBtn = document.getElementById("withdrowAmountBtn");
+withdrowBtn.addEventListener("click",function(){
+
+    const withdrowNum = getInputNumber("withdrowAmount");
+
+    updateSpanText("currentWithdrow", withdrowNum);
+
+    updateSpanText("currentBalance", -1* withdrowNum);
+
+
+    document.getElementById("withdrowAmount").value ="";
+
+})
+
+
+/*get input number form the transaction area*/
+
+function getInputNumber(id)
+{
+    const amount = document.getElementById(id).value;
+    const amountNumber =parseFloat(amount);
+    return amountNumber;
+}
+
+
+/*update span text*/
+
+function updateSpanText(id,addedNumber)
+{
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = addedNumber + currentNumber;
+    document.getElementById(id).innerText = totalAmount;
+}
+
 
